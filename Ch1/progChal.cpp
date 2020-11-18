a01e0c30d472c20dbeb6fc4bb431aaf727971cc6
DanielPCurtis <danieldevelopcurtis@gmail.com>
2020-11-18 11:40:36 -0800

    Daniel - finishing up Chapter 3 finally

--- Ch1/progChal.cpp
+++ Ch1/progChal.cpp
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <cmath>
 
 using namespace std;
 
@@ -12,23 +13,33 @@ void salesPrediction();
 void salesTax();
 void restaurantBill( double x );
 void stadiumSeating();
+void fiveTestAvg();
 void saveNum();
+void celsiusToFarenheit( double degrees);
+void interestEarned();
 
 int main()	{
 	/////////Chapter 2////////
 	//1. Sum of Two Numbers
-	sumTwoNum();
+	//sumTwoNum();
 	//2. Sales Prediction
-	salesPrediction();
+	//salesPrediction();
 	//3. Sales Tax
-	salesTax();
+	//salesTax();
 	//4. Restaurant Bill
-	restaurantBill( 44.50 );
+	//restaurantBill( 44.50 );
 	/////////Chapter 3////////
 	//2. Stadium Seating
-	stadiumSeating();
+	//stadiumSeating();
+	//3. Test Average
+	//fiveTestAvg();
+	//10. Celsius to Farenheit
+	//celsiusToFarenheit( 100.0 );
+	//16. Interest Earned
+	interestEarned();
+	//17. Monthly Payments
 	//21. Saving Numbers to a File
-	saveNum();
+	//saveNum();
 	
 	
 	
@@ -74,6 +85,76 @@ void restaurantBill( double x )	{
 	cout << "Grand Total: $" << setprecision(2) << fixed << total << "\n";
 }
 
+/* 	Write a program that asks how many tickets for each class of seats were sold
+	then displays the amount of income generated. Format your dollar amount to 
+	be foramtted in fixed-point notation with one decimal point of precision    	*/
+void displayIncome( int a, int b, int c )	{
+	double tots = a+b+c;
+	cout << setprecision(2) << fixed;
+	cout << "\nTotal Amount generated: " << tots << endl;
+	return;
+}	
+
+void stadiumSeating()	{
+	const double A = 15.0;
+	const double B = 12.0;
+	const double C = 9.0;
+	int totalA, totalB, totalC;
+	cout << "\nEnter how many tickets of Class A were sold: ";
+	cin >> totalA;
+	cout << "\nEnter how many tickets of Class B were sold: ";
+	cin >> totalB;
+	cout << "\nEnter how many tickets of Class C were sold: ";
+	cin >> totalC;
+	displayIncome(totalA * A, totalB * B, totalC * C);
+	return;
+}
+
+/*  Write a program that calculates the average test score for 5 test scores	*/
+void fiveTestAvg()	{
+	const double testTotal = 5.0;
+	double test1, test2, test3, test4, test5;
+	cout << "Enter 5 test scores: \n";
+	cin >> test1 >> test2 >> test3 >> test4 >> test5;
+	const double avg = (test1 + test2 + test3 + test4 + test5) / testTotal;
+	cout << setprecision(1) << fixed;
+	cout << "Test Average for these 5 foos is: " << avg << endl;
+	return;
+}
+
+/* 	Write a program that converts Celsius temperatures to Fahrenheit temperatures	*/
+void celsiusToFarenheit( double F )	{
+	double nineFifths = 1.8;
+	double celsius = nineFifths * F + 32;
+	cout << F << " degrees in Celsius is " << celsius << " degrees in Fahrenheit\n";
+	
+}
+
+/*   Write a program that asks for the principal, the interest rate, and the number of times
+	the interest is compounded during a year( T is 4 if the interest is compounded quarterly)  */
+void interestEarned()	{
+	//input from user: principal, interest rate, number of times compounded
+	double principal, interestRate;
+	int numCompounded;
+	cout << "\nEnter the total balance of the savings account: ";
+	cin >> principal;
+	cout << "\nEnter the interest rate for the savings account: ";
+	cin >> interestRate;
+	cout << "\nEnter the total number of times the interest has compounded: \n";
+	cin >> numCompounded;
+	cout << "\nReport\n\n";
+	double amt = principal * pow( (1 + ( interestRate/numCompounded ) ), numCompounded);
+	double interestTotal = amt - principal;
+	double formatInt = interestRate * 100;
+	cout << right;
+	cout << "Interest Rate: \t\t" << setw(8) << setprecision(2) << fixed << formatInt << "%" << endl;
+	cout << "Times Compounded: \t" << setw(8) << numCompounded << endl;
+	cout << setw(8) << setprecision(2) << showpoint;
+	cout << "Principal: \t\t$" << principal << endl;
+	cout << "Interest: \t\t$" << interestTotal << endl;
+	cout << "Amount in Savings: \t$" << amt << endl;
+	
+}
 /*	Wrte 2 programs:
 	Write a program that asks the user to enter five numbers. Create a file and save all five numbers
 	Write a program that opens the file created reads the five numbers and displays them, 
@@ -91,6 +172,7 @@ void enterFiveNumbers()	{
 	outputFile  << d << endl;
 	outputFile  << e << endl;
 	outputFile.close();
+	cout << "\nFile has been created and numbers added...";
 	return;
 }
 
@@ -98,7 +180,7 @@ void printFiveNumbers()	{
 	ifstream inputFile;
 	inputFile.open( "fiveNum.txt" );
 	int num;
-	
+	cout << "\nNow we are gonna print them numbies!\n";
 	inputFile >> num;
 	cout << num <<"\n";
 	inputFile >> num;
